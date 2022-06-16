ec55f6df354632ea5a70f3feeaa2756faa87bb10
Timo Glastra <timo@animo.id>
2022-06-16 11:45:57 +0200

    error
    Signed-off-by: Timo Glastra <timo@animo.id>

--- src/example/exampleController.ts
+++ src/example/exampleController.ts
@@ -1,20 +1,50 @@
 import { Body, Controller, Post, Route } from "tsoa";
 
-export interface ThePayload {
-  name: string;
+export interface Format {
+  key: string;
+  value: unknown;
 }
 
-export interface TheInterface<T> {
-  thePayload: T;
+export interface ExampleFormat extends Format {
+  key: "example";
+  value: {
+    theValue: string;
+  };
 }
+
+interface CreateUserRequest {
+  formats: FormatPayload<[ExampleFormat]>;
+}
+
+/**
+ * Get the payload from a list of Format interfaces
+ *
+ * @example
+ * ```
+ *
+ * type CreateOfferFormat = FormatPayload<[ExampleFormat]>
+ *
+ * // equal to
+ * type CreateOfferFormat = {
+ *  example: {
+ *   theValue: string;
+ *  }
+ * }
+ * ```
+ */
+export declare type FormatPayload<Fs extends Format[]> = {
+  [Format in Fs[number] as Format["key"]]?: Format["value"];
+};
 
 @Route("example")
 export class ExampleController extends Controller {
   @Post()
   public async createUser(
     @Body()
-    requestBody: TheInterface<ThePayload>
+    requestBody: CreateUserRequest
   ) {
-    return requestBody;
+    console.log(requestBody);
+
+    return 10;
   }
 }
